da674df2f952952e6f756273cd454488d2812b8c
Savlong <soulianicet1@gmail.com>
2025-11-03 14:22:31 +0000

    kubernetes

--- gestion-hospitaliere-frontend/src/services/api.js
+++ gestion-hospitaliere-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api';
 console.log('API_BASE_URL:', API_BASE_URL);
 
 const api = axios.create({
